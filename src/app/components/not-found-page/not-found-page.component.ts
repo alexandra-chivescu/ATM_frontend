e92716424ca10680a9a85715e574b787599ab208
ALEXANDRACHIVESCU <alechivescu@gmail.com>
2022-09-12 10:48:46 +0300

    - changed addClient method from service - added 404(not found) page

---
+++ src/app/components/not-found-page/not-found-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Data} from "@angular/router";
+
+@Component({
+  selector: 'app-not-found-page',
+  templateUrl: './not-found-page.component.html',
+  styleUrls: ['./not-found-page.component.css']
+})
+export class NotFoundPageComponent {
+
+}
